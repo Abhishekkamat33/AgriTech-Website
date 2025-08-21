578afe988f250bcfa618c8c365d86d9bff588c2b
abhishek <abhishekkamat33@gmail.com>
2025-08-21 22:32:31 +0545

    complete setup

--- src/main/java/com/adhunikkethi/adhunnikkethi/AdhunnikkethiApplication.java
+++ src/main/java/com/adhunikkethi/adhunnikkethi/AdhunnikkethiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.adhunikkethi.adhunnikkethi.repositories")
+@EnableJpaRepositories(basePackages = "com.adhunikkethi.adhunnikkethi.Respository")
 public class AdhunnikkethiApplication {
 
 	public static void main(String[] args) {
